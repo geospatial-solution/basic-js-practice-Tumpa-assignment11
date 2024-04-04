74513b2b7758332fd65c5759c089c75b7cf0aa49
geospatial-solution <158801299+geospatial-solution@users.noreply.github.com>
2024-04-04 11:02:49 +0600

    Add files via upload

---
+++ class11.js
@@ -0,0 +1,27 @@
+// Make a NDVI map of your Upazila by using Landsat 9 imagery for 2022. Upload the code, code link,
+//  and screenshot in your GitHub repo. Submit the repo link.
+
+print(roi.first())
+
+var upazila = roi.filter(ee.Filter.eq("NAME_3","Fatikchhari"))
+print(upazila)
+Map.addLayer(upazila,{}, "Fatikchhari")
+// Map.centerObject(upazila)
+        
+var s2= ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+        .filterBounds(roi)
+        .filterDate('2022-01-01', '2022-01-30')
+        .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20))
+        .first()
+        .select(["B2","B4","B3","B8"])
+       
+Map.addLayer(s2.clip(roi),{},"RGB image")
+print(s2)
+var ndvi = s2.normalizedDifference(["B8","B4"]).rename("NDVI")
+print(ndvi,"ndvi image")
+var vizParam = {
+  min: -1,
+  max: 1,
+  palette: ["blue","cyan","green"]
+}
+Map.addLayer(ndvi.clip(roi),vizParam,"NDVI image")
